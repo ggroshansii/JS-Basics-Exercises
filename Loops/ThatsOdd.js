40c80e5695cd4a08176d810fc5e3fb3b44bc856d
ggroshansii <garthgroshansii@gmail.com>
2021-05-07 08:43:13 -0400

    Loops exercises complete

---
+++ Loops/ThatsOdd.js
@@ -0,0 +1,13 @@
+/*
+
+Write a while loop that logs all odd natural numbers between 1 and 40.
+
+*/
+
+let index = 0;
+while (index <= 40){
+    if (index % 2 !== 0){
+        console.log(index);
+    }
+    index++;
+}
---
+++ Loops/TripleGreeting.js
@@ -0,0 +1,11 @@
+/*
+
+Write a loop that logs greeting three times.
+
+*/
+
+let greeting = 'Aloha!';
+
+for (i=0; i < 3; i++){
+    console.log(greeting);
+}
